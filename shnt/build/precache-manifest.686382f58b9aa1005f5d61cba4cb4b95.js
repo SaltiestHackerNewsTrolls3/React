95053bb8e0674f9fa4dc4f2ea20f8e2fc4f2cc93
bret-knight87 <bret.knight87@gmail.com>
2020-01-13 21:53:30 -0600

    run npm build for netlify

---
+++ shnt/build/precache-manifest.686382f58b9aa1005f5d61cba4cb4b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2536f00878b2c69d152284a7d81e597b",
+    "url": "/Front-End/index.html"
+  },
+  {
+    "revision": "dd5c62b079a2f394be79",
+    "url": "/Front-End/static/css/main.0c5f54f9.chunk.css"
+  },
+  {
+    "revision": "3603d10c54eabbd0910c",
+    "url": "/Front-End/static/js/2.de1b2519.chunk.js"
+  },
+  {
+    "revision": "b2f3f3632aa8113739d5f40daae9c618",
+    "url": "/Front-End/static/js/2.de1b2519.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd5c62b079a2f394be79",
+    "url": "/Front-End/static/js/main.eff1d1fe.chunk.js"
+  },
+  {
+    "revision": "5ca7c66fcf9a49330874",
+    "url": "/Front-End/static/js/runtime-main.8d2969d2.js"
+  }
+]);
